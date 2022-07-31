94ce693890cb34a0a96fca3b76b9180a9bef4169
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-07-31 03:11:49 -0300

    Refatoramento
    Adicionando um metodo para criar botoes, inves de ficar criando um por um.

--- calculadora/main.py
+++ calculadora/main.py
@@ -9,9 +9,6 @@ class Janela(QMainWindow):
     def __init__(self):
         super().__init__()  # Herdando da classe para configurar janela
 
-        self.digitou = ''  # Oque o usuario digitou
-        self.resultado = 0  # Resultado do calculo
-
         # Tamanho, largura e titulo da janela
         self.topo = 450
         self.esquerda = 120
@@ -19,163 +16,73 @@ class Janela(QMainWindow):
         self.largura = 250
         self.titulo = 'Calculadora'
 
-        # Criando cada botao e colocando na tela, cada botao chama uma funcao...
-        self.numero0 = QPushButton('0', self)  # Botao 0
-        self.numero0.move(60, 200)  # Posicao do botao na janela
-        self.numero0.resize(60, 50)  # Altura e largura do botao
-        self.numero0.clicked.connect(self.numero0_clicado)  # Funcao que o botao chama a ser clicado
+        # Criando cada botao e colocando na tela em uma posicao especifica.
+        self.botao('C', 0, 200, clear=True)  # Informando que é o botao de limpar
+        self.botao('0', 60, 200)
+        self.botao('=', 120, 200, igual=True)  # Informando que é o botao para ver o resultado
+        self.botao('+', 180, 200)
 
-        self.numero1 = QPushButton('1', self)
-        self.numero1.move(0, 150)
-        self.numero1.resize(60, 50)
-        self.numero1.clicked.connect(self.numero1_clicado)
+        self.botao('1', 0, 150)
+        self.botao('2', 60, 150)
+        self.botao('3', 120, 150)
+        self.botao('-', 180, 150)
 
-        self.numero2 = QPushButton('2', self)
-        self.numero2.move(60, 150)
-        self.numero2.resize(60, 50)
-        self.numero2.clicked.connect(self.numero2_clicado)
+        self.botao('4', 0, 100)
+        self.botao('5', 60, 100)
+        self.botao('6', 120, 100)
+        self.botao('x', 180, 100)
 
-        self.numero3 = QPushButton('3', self)
-        self.numero3.move(120, 150)
-        self.numero3.resize(60, 50)
-        self.numero3.clicked.connect(self.numero3_clicado)
-
-        self.numero4 = QPushButton('4', self)
-        self.numero4.move(0, 100)
-        self.numero4.resize(60, 50)
-        self.numero4.clicked.connect(self.numero4_clicado)
-
-        self.numero5 = QPushButton('5', self)
-        self.numero5.move(60, 100)
-        self.numero5.resize(60, 50)
-        self.numero5.clicked.connect(self.numero5_clicado)
-
-        self.numero6 = QPushButton('6', self)
-        self.numero6.move(120, 100)
-        self.numero6.resize(60, 50)
-        self.numero6.clicked.connect(self.numero6_clicado)
-
-        self.numero7 = QPushButton('7', self)
-        self.numero7.move(0, 50)
-        self.numero7.resize(60, 50)
-        self.numero7.clicked.connect(self.numero7_clicado)
-
-        self.numero8 = QPushButton('8', self)
-        self.numero8.move(60, 50)
-        self.numero8.resize(60, 50)
-        self.numero8.clicked.connect(self.numero8_clicado)
-
-        self.numero9 = QPushButton('9', self)
-        self.numero9.move(120, 50)
-        self.numero9.resize(60, 50)
-        self.numero9.clicked.connect(self.numero9_clicado)
-
-        self.clear = QPushButton('C', self)
-        self.clear.move(0, 200)
-        self.clear.resize(60, 50)
-        self.clear.clicked.connect(self.clear_clicado)
-
-        self.igual = QPushButton('=', self)
-        self.igual.move(120, 200)
-        self.igual.resize(60, 50)
-        self.igual.clicked.connect(self.igual_clicado)
-
-        self.mais = QPushButton('+', self)
-        self.mais.move(180, 200)
-        self.mais.resize(60, 50)
-        self.mais.clicked.connect(self.mais_clicado)
-
-        self.menos = QPushButton('-', self)
-        self.menos.move(180, 150)
-        self.menos.resize(60, 50)
-        self.menos.clicked.connect(self.menos_clicado)
-
-        self.x = QPushButton('X', self)
-        self.x.move(180, 100)
-        self.x.resize(60, 50)
-        self.x.clicked.connect(self.x_clicado)
-
-        self.dividir = QPushButton('÷', self)
-        self.dividir.move(180, 50)
-        self.dividir.resize(60, 50)
-        self.dividir.clicked.connect(self.dividir_clicado)
+        self.botao('7', 0, 50)
+        self.botao('8', 60, 50)
+        self.botao('9', 120, 50)
+        self.botao('÷', 180, 50)
 
         # Caixa de texto onde aparecera os numeros digitados
         self.digitados = QLineEdit(self)
         self.digitados.move(0, 0)
         self.digitados.resize(239, 50)
-        self.digitados.setStyleSheet('font-size: 40px; font-family: Arial, Helvetica, sans-serif')  # Personalizando com css
+        # Personalizando com css
+        self.digitados.setStyleSheet('font-size: 40px; font-family: Arial, Helvetica, sans-serif')
 
         self.carregar_janela()  # Carregando a janela
 
-    def numero0_clicado(self):  # Funcao do botao ao ser clicado
-        self.digitou += '0'
-        self.digitados.setText(self.digitou)  # Mostrando na janela o valor do botao
+    def botao(self, botao, d_esquerda, d_topo, igual=None, clear=None):
+        """
+        Metodo que ira criar um botao e mostra-lo na janela. Se for criado o botao de igualdade ( = ) ou o botao de
+        limpar a tela ( C ), eles devem ser especificados com o parametro igual/clear recebendo True, para o botao que
+        foi criado.
 
-    def numero1_clicado(self):
-        self.digitou += '1'
-        self.digitados.setText(self.digitou)
+        :param botao: Nome do botao
+        :param d_esquerda: Quantidade de pixels de distancia que ira ficar do lado esquerdo da janela
+        :param d_topo:  Quantidade de pixels de distancia que ira ficar do topo da janela
+        :param igual: Parametro para saber se é o botao de mostrar resultado ou nao, se for recebe True
+        :param clear: Parametro para saber se é o botao de limpar tela ou nao, se for recebe True
+        :return: None
+        """
+        # Criando botao
+        botao_criado = QPushButton(botao, self)
+        botao_criado.move(d_esquerda, d_topo)
+        botao_criado.resize(60, 50)
 
-    def numero2_clicado(self):
-        self.digitou += '2'
-        self.digitados.setText(self.digitou)
+        if not igual and not clear:  # Se o botao for um numero
+            botao_criado.clicked.connect(lambda: self.digitados.setText(self.digitados.text() + botao))
 
-    def numero3_clicado(self):
-        self.digitou += '3'
-        self.digitados.setText(self.digitou)
+        else:
+            if igual:
+                botao_criado.clicked.connect(self.igual_clicado)  # Chamara a funcao 'igual_clicado'
 
-    def numero4_clicado(self):
-        self.digitou += '4'
-        self.digitados.setText(self.digitou)
-
-    def numero5_clicado(self):
-        self.digitou += '5'
-        self.digitados.setText(self.digitou)
-
-    def numero6_clicado(self):
-        self.digitou += '6'
-        self.digitados.setText(self.digitou)
-
-    def numero7_clicado(self):
-        self.digitou += '7'
-        self.digitados.setText(self.digitou)
-
-    def numero8_clicado(self):
-        self.digitou += '8'
-        self.digitados.setText(self.digitou)
-
-    def numero9_clicado(self):
-        self.digitou += '9'
-        self.digitados.setText(self.digitou)
-
-    def mais_clicado(self):
-        self.digitou += '+'
-        self.digitados.setText(self.digitou)
-
-    def menos_clicado(self):
-        self.digitou += '-'
-        self.digitados.setText(self.digitou)
-
-    def x_clicado(self):
-        self.digitou += 'x'
-        self.digitados.setText(self.digitou)
-
-    def dividir_clicado(self):
-        self.digitou += '÷'
-        self.digitados.setText(self.digitou)
-
-    def clear_clicado(self):
-        self.digitou = ''
-        self.digitados.setText(self.digitou)
+            if clear:
+                botao_criado.clicked.connect(lambda: self.digitados.setText(''))  # Limpando tela da calculadora
 
     def igual_clicado(self):  # Se o botao igual foi clicado para ver o resultado do calculo
         valor = ''  # Variavel onde ira ficar cada valor digitado
         calculo = []
 
-        for p, c in enumerate(self.digitou):
+        for p, c in enumerate(self.digitados.text()):
             if c.isdigit():  # Se o caractere da vez for um numero
                 valor += c  # Concatenando
-                if p == len(self.digitou) - 1:  # Se o caractere da vez for o ultimo
+
+                if p == len(self.digitados.text()) - 1:  # Se o caractere da vez for o ultimo
                     calculo.append(valor)  # Adicionando valor
                     valor = ''  # Limpando variavel para o proximo valor
 
@@ -187,9 +94,8 @@ class Janela(QMainWindow):
         resultado = 0  # Variavel que aparecera o resultado
 
         if len(calculo) > 3:  # Se o usuario digitou mais de um calculo ao mesmo tempo
-            self.digitou = ''
             self.digitados.setStyleSheet('font-size: 15px')  # Diminuindo tamanho da fonte
-            self.digitados.setText('So fazemos um calculo por vez.')  # Mostrando uma mensagem
+            self.digitados.setText('Um calculo por vez.')  # Mostrando uma mensagem
 
         else:
             for i, e in enumerate(calculo):
@@ -202,9 +108,8 @@ class Janela(QMainWindow):
                 if e == '÷':
                     resultado = int(calculo[i - 1]) / int(calculo[i + 1])
 
-            self.digitou = str(resultado)
             self.digitados.setStyleSheet('font-size: 40px')
-            self.digitados.setText(self.digitou)
+            self.digitados.setText(str(resultado))
 
     def carregar_janela(self):
         self.setGeometry(self.topo, self.esquerda, self.altura, self.largura)  # Aplicando tamanho e largura colocados
